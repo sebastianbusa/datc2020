6b8c247fd657fe20e124b0e4a3c6722c0b181706
sebastianbusa <73118310+sebastianbusa@users.noreply.github.com>
2020-10-26 15:45:25 +0200

    Add files via upload

---
+++ BusaSebastian/Tema3_DATC/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Newtonsoft.Json.Linq;
+
+namespace Tema3
+{
+    class Program
+    {
+
+        private static DriveService _service;
+        private static string _token;
+
+        static void Main(string[] args)
+        {
+            init();
+        }
+
+    static void init()
+    {
+        string[] scop = new string[]
+        {
+            DriveService.Scope.Drive,
+            DriveService.Scope.DriveFile
+        };
+
+        var clientId="914728126287-co0g1dc07h6sfnq2fqchd3pl1i6ovv4u.apps.googleusercontent.com";
+        var clientSecret="aUPmh5BMSJjdXrTTDlmNrDB-";
+        
+           var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = clientId,
+                    ClientSecret = clientSecret
+                },
+                scop,
+                Environment.UserName,
+                CancellationToken.None,
+
+                null
+
+            ).Result;
+
+        _service = new DriveService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential
+            });
+    _token=credential.Token.AccessToken;
+
+
+    Console.Write("Token: "+credential.Token.AccessToken);
+
+
+    }
+
+    }
+}
